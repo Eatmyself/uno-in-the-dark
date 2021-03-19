1ba16e3081d274790960b0c26298a0a9aec87b61
Eatmyself <nagin2358@gmail.com>
2021-03-19 18:47:00 +0800

    Initial Commit

---
+++ uno/unoApp.swift
@@ -0,0 +1,17 @@
+//
+//  unoApp.swift
+//  uno
+//
+//  Created by Eatmyself on 2021/3/19.
+//
+
+import SwiftUI
+
+@main
+struct unoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
